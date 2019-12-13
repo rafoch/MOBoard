b35ec105db8c0b5c8d308b417c36ae7e2ee3bbf3
Rafał Ochotny <rafal.ochotny95@gmail.com>
2019-12-13 21:45:02 +0100

    added fake test

---
+++ UnitTests/BasicTest.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace MOBoard.UnitTests
+{
+    public class BasicTest
+    {
+        [Fact]
+        public void FakeAssertion()
+        {
+            Assert.True(true);
+        }
+    }
+}
