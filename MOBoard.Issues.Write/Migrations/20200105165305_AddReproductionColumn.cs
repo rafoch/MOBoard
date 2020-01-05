c3239b510fd89288d49f8b4c89064b5883906e6d
Greg <grzp91@gmail.com>
2020-01-05 17:58:23 +0100

    Add migration for reproduction

---
+++ MOBoard.Issues.Write/Migrations/20200105165305_AddReproductionColumn.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MOBoard.Issues.Write.Migrations
+{
+    public partial class AddReproductionColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Reproduction",
+                schema: "Issue",
+                table: "Issues",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Reproduction",
+                schema: "Issue",
+                table: "Issues");
+        }
+    }
+}
